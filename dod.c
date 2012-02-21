53a5e0c16faba749b269baa99ee49db32a6f0260
Tomasch <tomasch@bt.foo.org>
2012-02-21 14:41:22 +0100

    początki

---
+++ dod.c
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+
+struct lista l {
+	int a;
+	int baza;
+	struct lista *l;
+};
+
+void dodaj(int a, struct lista l) {
+	if(a+l.a>=baza) {
+		if(*l) {
+			l.l=calloc()
+		}
+	}
+
+}
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
+
